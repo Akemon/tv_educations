1e1b238a1b407c3fa34c4312791dc1a998da2fba
allen <597724030@qq.com>
2017-08-22 17:41:08 +0800

    student teacher school admin dao

--- src/main/java/application/com/dao/NumberProviceDao.java
+++ src/main/java/application/com/dao/NumberProviceDao.java
@@ -13,12 +13,13 @@ public class NumberProviceDao {
     public List<NumberProvice> countNumberProvince(){
         studentDao sd=new studentDao();
         teacherDao td=new teacherDao();
-
+        List<NumberProvice> p1=sd.countNumberProvice();
+        List<NumberProvice> p2=td.countNumberProvice();
 
         List<NumberProvice> ls=new ArrayList<NumberProvice>();
         String[] province={"北京市","天津市","上海市","重庆市","河北省","山西省","辽宁省","吉林省","黑龙江省","江苏省","浙江省","安徽省","福建省","江西省","山东省","河南省","湖北省","湖南省","广东省","海南省","四川省","贵州省","云南省","陕西省","甘肃省","青海省","台湾省","内蒙古自治区","广西壮族自治区","西藏自治区","宁夏回族自治区","新疆维吾尔自治区","香港特别行政区","澳门特别行政区"} ;
         for(int i=0;i<34;i++){
-            NumberProvice np=new NumberProvice(province[i],sd.countNumberProvice().get(i).getPeopleNumber()+ td.countNumberProvice().get(i).getPeopleNumber());
+            NumberProvice np=new NumberProvice(province[i],p1.get(i).getPeopleNumber()+ p2.get(i).getPeopleNumber());
             System.out.println(np.getProvince()+":"+np.getPeopleNumber());
             ls.add(np);
     }
